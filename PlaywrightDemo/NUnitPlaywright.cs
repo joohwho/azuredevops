05b333066d68c65dd0a722c168e675cf0c76d895
Jonathan Oliveira <jonathan.oliveira@fit-tecnologia.org.br>
2023-07-08 08:08:28 -0300

    The first test build and test run azure

--- PlaywrightDemo/NUnitPlaywright.cs
+++ PlaywrightDemo/NUnitPlaywright.cs
@@ -16,19 +16,29 @@ public class NUnitPlaywright
     public async Task Test1()
     {
         using var playwright = await Playwright.CreateAsync();
+        Console.WriteLine("CreateAsync");
         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
             Headless = false
         });
+        Console.WriteLine("LaunchAsync");
         var page = await browser.NewPageAsync();
+        Console.WriteLine("NewPageAsync");
 
         await page.GotoAsync("http://www.eaapp.somee.com");
+        Console.WriteLine("GotoAsync");
 
         LoginPage loginPage = new LoginPage(page);
+        Console.WriteLine("LoginPage");
         await loginPage.ClickLogin();
+        Console.WriteLine("ClickLogin");
         await loginPage.Login("admin", "password");
+        Console.WriteLine("Login");
         bool isExist = await loginPage.IsEmployeeDetailsExists();
+        Console.WriteLine("IsEmployeeDetailsExists");
 
         Assert.IsTrue(isExist);
+        Console.WriteLine("Assert.IsTrue");
+        Console.WriteLine("Passed!");
     }
 }
